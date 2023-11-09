78f70246508cbdcf8d271fcb70d2eb9315940d4b
Renaud-HUSSON <renaudhusson09@gmail.com>
2023-11-09 11:20:21 +0100

    feat: use prisma singleton

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,11 @@
+import prisma from "@/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
   throw new Error("Missing GITHUB_ID or GITHUB_SECRET environment variable");
 }
-
-const prisma = new PrismaClient();
 
 export const authConfig = NextAuth({
   adapter: PrismaAdapter(prisma),
