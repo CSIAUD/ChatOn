e8bada56e8d389a0579b140f9127a172d9addc47
Jules Bloux <jules.bloux@innovia-france.com>
2023-11-09 09:59:16 +0100

    sideHeader v0

--- src/components/SideHeader.tsx
+++ src/components/SideHeader.tsx
@@ -1,3 +1,24 @@
+import GroupHead from "@/components/GroupHead";
+import Groupe from "@/model/Groupe";
+
 export default function SideHeader() {
-  return <></>;
+  const tabMessages = [];
+  const tabGroupes: Array<Groupe> = [];
+
+  const handleClick = () => {};
+
+  return (
+    <div>
+      <button onClick={handleClick}>+</button>
+      {tabGroupes.map((groupe, index) => {
+        return (
+          <GroupHead
+            key={index}
+            lastMessage={groupe.lastMessage}
+            unseenMessagesNumber={0}
+          />
+        );
+      })}
+    </div>
+  );
 }
