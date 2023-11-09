dbb01363fe6b4d43624fbada0d33b88135e388b3
Cyprien Siaud <cyprien.siaud@ynov.com>
2023-11-09 11:07:49 +0100

    clean socket

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -2,17 +2,15 @@ import { Server } from 'Socket.IO'
 
 const SocketHandler = (req, res) => {
   if (res.socket.server.io) {
-    console.log('Socket is already running')
+    console.log('Socket is already running') // log dans CMD
   } else {
-    console.log('Socket is initializing')
+    console.log('Socket is initializing') // log dans CMD
     const io = new Server(res.socket.server)
     res.socket.server.io = io
 
     io.on('connection', socket => {
         socket.on('form-submit', msg => {
-            console.log("form-submit", msg);
-            
-            socket.broadcast.emit('form-submit', msg)
+            socket.broadcast.emit('form-submit', msg) // Message de retour en direction du front
         })
     })
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,8 @@
-import { useEffect, useState } from 'react'
-import io from 'Socket.IO-client'
-let socket: any;
-
 const Home = () => {
-  const [input, setInput] = useState('')
-
-  useEffect(() => {socketInitializer()}, [])
-
-  const socketInitializer = async () => {
-    await fetch('/api/socket');
-    socket = io()
-
-    socket.on('connect', () => {
-      console.log('connected')
-    })
-
-    socket.on('form-submit', (msg: string) => {
-      setInput(msg)
-    })
-  }
-
-  const onChangeHandler = (e: any) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
-  const onEmitHandler = (e: any) => {
-    e.preventDefault();
-    let input = ([].slice.call(e.target.children)).find((item: HTMLElement) => item.id === "text-input") as HTMLInputElement | undefined;
-    if(input){
-      setInput(input.value || "")
-      socket.emit('form-submit', input.value)
-    }else{
-      console.log("input undefined");
-      
-    }
-  }
 
   return (
-    <form action="" onSubmit={onEmitHandler}>
-      <input
-        placeholder="Type something"
-        value={input}
-        onChange={onChangeHandler}
-        id="text-input"
-      />
-      <input type="submit" value="Envoyer" />
-    </form>
+    <>
+    </>
   )
 }
 
